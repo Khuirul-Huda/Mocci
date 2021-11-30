0fcf1121e1cf57b0ef7d8c345ef87636e5ed1d63
Khuirul-Huda <hudah2102@gmail.com>
2021-11-30 22:09:51 +0700

    remove ver field

--- interactions/ping.js
+++ interactions/ping.js
@@ -5,30 +5,25 @@ const os = require("os")
 //System properties
 const hostname = os.hostname()
 const platform = os.platform()
-const ver = os.version()
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with pong'),
 
-
-		
 	async execute(interaction) {
 		const time = Date.now() - interaction.createdTimestamp
 		const about = new MessageEmbed()
-						.setColor('#31ff08')
-						.setTitle('About Mocci Bot')
-						.setDescription('AFAIK Mocci Bot is under development:)')
-						.setImage('https://i.postimg.cc/XvQVPfGP/free-anime-image.jpg')
-						
-						.addFields(
-							{name: 'Hostname', value: 'Current: '+hostname},
-							{name: 'Platform', value: 'Current:'+platform.toString},
-							{name: 'Version', value: 'Current:'+ver},
-							{name: 'Current Processing Time', value: 'Current:'+time.toString}
-						);
+			.setColor('#31ff08')
+			.setTitle('About Mocci Bot')
+			.setDescription('AFAIK Mocci Bot is under development')
+			.setImage('https://i.postimg.cc/XvQVPfGP/free-anime-image.jpg')
 
+			.addFields(
+				{ name: 'Hostname', value: 'Current\t' + hostname },
+				{ name: 'Platform', value: 'Current\t' + platform.toString },
+				{ name: 'Current Processing Time', value: 'Current\t' + time.toString }
+			);
 
 		interaction.reply({ content: [about] })
 	}

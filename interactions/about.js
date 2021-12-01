2c257e3e16a9d65be8cc19e5696b481582b75e8f
Khuirul-Huda <hudah2102@gmail.com>
2021-12-01 17:22:09 +0700

    update about.js

--- interactions/about.js
+++ interactions/about.js
@@ -1,4 +1,3 @@
-
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 const os = require("os")
@@ -10,24 +9,24 @@ const cpus = os.cpus()
 const cputotal = os.cpus().length
 const model = cpus[0].model
 
-let uptime = new Date(os.uptime * 1000).toISOString().substr(11, 8)
-let freemem = `${os.freemem()} Bytes`
-let totalmem = `${os.totalmem()} Bytes`
-let memoryinfo = `${freemem} / ${totalmem}`
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('about')
 		.setDescription('Show detailed Mocci Bot Info'),
 
 	async execute(interaction) {
+		const uptime = new Date(os.uptime * 1000).toISOString().substr(11, 8)
+		const freemem = `${os.freemem()}`
+		const totalmem = `${os.totalmem()} Bytes`
+		const memoryinfo = `${freemem} / ${totalmem}`
+
 		const time = Date.now() - interaction.createdTimestamp
 		const sysinfo = `**Hostname:** ${hostname}\n**Platform:** ${platform}\n**Header:** ${headerversion}\n**Cpu:** ${model} x${cputotal}\n**Memory:** ${memoryinfo} \n**Uptime:** ${uptime}\n\nDelay Interact: ${time} ms\n Source code: [here](https://github.com/Khuirul-Huda/Mocci)`
 
 		const about = new MessageEmbed()
 			.setColor('#31ff08')
 			.setTitle('About')
-			.setDescription('Ini adalah sebuah random text yang digunakan untuk mengisi deskripsi hehe hehe hehe abc cdej ')
+			.setDescription('Ini adalah sebuah random text yang digunakan\n untuk mengisi deskripsi hehe hehe hehe abc cdej ')
 			.setImage('https://i.postimg.cc/XvQVPfGP/free-anime-image.jpg')
 			.setFooter('Support us /support')
 			.addFields(

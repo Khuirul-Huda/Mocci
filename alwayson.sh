eaf21e91171eeb76bb7f01ed89b60cac6d12c53e
Khuirul-Huda <hudah2102@gmail.com>
2021-12-02 15:25:50 +0700

    edit script :)

--- alwayson.sh
+++ alwayson.sh
@@ -1,7 +1,8 @@
-while ( true ) {
+while true
+do
     npm run deploy
     echo "Waiting 60 seconds before next restart... press CTRL+C to cancel"
     sleep 60
     clear
     echo "New Session"
-}
+done

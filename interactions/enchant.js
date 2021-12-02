7796c90f9c7e35cdadcd57d8971234381eb3a8cd
Khuirul-Huda <hudah2102@gmail.com>
2021-12-02 14:30:43 +0700

    fix error when return null

--- interactions/enchant.js
+++ interactions/enchant.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 const { SlashCommandBuilder, SlashCommandStringOption } = require('@discordjs/builders');
 const Enchant = require("enchantment-table")
-
+let hide = false
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('enchant')
-		.setDescription('Translates any text to enchanting table language')
+		.setDescription('Translates any text (not number) to enchanting table language')
 		.addStringOption(option => option.setName("text")
 			.setRequired(true)
 			.setDescription("Text to translate")
@@ -13,6 +13,12 @@ module.exports = {
 	async execute(interaction) {
 		let untranslatedText = interaction.options.data.find(arg => arg.name === 'text').value
 		let translatedText = Enchant.translate(untranslatedText)
-		interaction.reply({ content: translatedText, ephemeral: false })
+		
+		if (translatedText === "" ) {
+			let translatedText = "We need something to reply"
+			let hide = true
+		}
+
+		interaction.reply({ content: translatedText, ephemeral: hide })
 	}
 };

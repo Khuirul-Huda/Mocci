9feb78d237e5fe4cdd749b5f4e36883d0550fac6
Khuirul-Huda <hudah2102@gmail.com>
2021-12-11 13:22:22 +0700

    Change delay loop to 10 sec

--- alwayson.sh
+++ alwayson.sh
@@ -1,8 +1,8 @@
 while true
 do
     npm run deploy
-    echo "Waiting 60 seconds before next restart... press CTRL+C to cancel"
-    sleep 60
+    echo "Waiting 10 seconds before next restart... press CTRL+C to cancel"
+    sleep 10
     clear
     echo "New Session"
 done
